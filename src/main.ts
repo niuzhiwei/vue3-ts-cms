bbb2ed226e38448f62cfb702e6cbac5d52c4d854
niuzhiwei <xujin02@qq.com>
2021-10-13 14:12:58 +0800

    feat: add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import { registerApp } from './global'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(registerApp)
+app.use(store).use(router).mount('#app')
